5d998d1647203f5c9109bfb7665f67c5b336fcf9
田中克治 <tanakayoshiharu@tanakakatsujinoMacBook-Air.local>
2026-04-12 12:06:56 +0900

    a

--- js/main.js
+++ js/main.js
@@ -1,17 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const faqItems = document.querySelectorAll(".faq-item");
-
-  faqItems.forEach((item) => {
-    const question = item.querySelector(".faq-question");
-
-    question.addEventListener("click", () => {
-      faqItems.forEach((otherItem) => {
-        if (otherItem !== item) otherItem.classList.remove("active");
-      });
-      item.classList.toggle("active");
-    });
-  });
-
   // ===== ハンバーガー =====
   const toggleBtn = document.querySelector(".toggle-btn");
   const header = document.getElementById("header");
@@ -30,5 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
     link.addEventListener("click", () => {
       header.classList.remove("open");
     });
+  });
+
+    // ===== FAQ =====
+  $(".faq-question").on("click", function () {
+    const item = $(this).closest(".faq-item");
+    const answer = item.find(".faq-answer");
+
+    item.toggleClass("active");
+    answer.slideToggle(1000);
   });
 });
